efa3bdbe0466f98b86dc75bab04d3a4436eb1c2f
Helbin A. Perez <helbinalexander1@hotmail.com>
2020-01-31 21:46:06 -0400

    Prueba para subir

---
+++ DiaMes/Controllers/DiaMesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiaMes.Controllers
+{
+    public class DiaMesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Dato()
+        {
+            return View();
+        }
+    }
+}
